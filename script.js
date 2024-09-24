cb6921eb70f86af20f8bba94e87bf08c0efa4357
berktutu <166663599+berktutu@users.noreply.github.com>
2024-09-24 12:45:13 +0200

    refactored the code

--- script.js
+++ script.js
@@ -1,49 +1,47 @@
 const button = document.querySelectorAll(".btn");
-const buttonSmall = document.querySelector(".btn-small");
-const buttonMedium = document.querySelector(".btn-medium");
-const buttonLarge = document.querySelector(".btn-large");
 const canvas = document.querySelector(".canvas-container");
 
 // Create a webpage with a 16x16 grid of square divs.
-const smallCanvas = 16 * 16;
-const mediumCanvas = 24 * 24;
-const largeCanvas = 32 * 32;
+const canvasSetup = {
+  gridDimensions: {
+    small: 16,
+    medium: 24,
+    large: 32,
+  },
+};
 
-const smallCanvasDivs = 16;
-const mediumCanvasDivs = 24;
-const largeCanvasDivs = 32;
-
-const fillDivs = function (size, numberOfDivs) {
-  for (let i = 0; i < size; i++) {
+const fillDivs = function (size) {
+  const numberOfDivs = size * size;
+  for (let i = 0; i < numberOfDivs; i++) {
     const div = document.createElement("div");
-    div.style.flexBasis = `calc(100% / ${numberOfDivs})`;
-    div.style.height = `calc(100% / ${numberOfDivs})`;
+    div.classList.add("canvas-div");
+    div.style.flexBasis = `calc(100% / ${size})`;
+    div.style.height = `calc(100% / ${size})`;
     canvas.appendChild(div);
-
-    div.addEventListener("mouseover", function () {
-      div.style.backgroundColor = "black";
-    });
   }
 };
-fillDivs(smallCanvas, smallCanvasDivs);
+
+canvas.addEventListener("mouseover", function (event) {
+  if (event.target.classList.contains("canvas-div")) {
+    event.target.style.backgroundColor = "black";
+  }
+});
+
+fillDivs(canvasSetup.gridDimensions.small);
 
 const clearCanvas = function () {
   canvas.innerHTML = "";
 };
 
-button.forEach((btn, i) =>
+const gridSizes = [
+  canvasSetup.gridDimensions.small,
+  canvasSetup.gridDimensions.medium,
+  canvasSetup.gridDimensions.large,
+];
+
+button.forEach((btn, btnIndex) => {
   btn.addEventListener("click", function () {
-    if (btn) {
-      clearCanvas();
-    }
-    if (i === 0) {
-      fillDivs(smallCanvas, smallCanvasDivs);
-    }
-    if (i === 1) {
-      fillDivs(mediumCanvas, mediumCanvasDivs);
-    }
-    if (i === 2) {
-      fillDivs(largeCanvas, largeCanvasDivs);
-    }
-  })
-);
+    clearCanvas();
+    fillDivs(gridSizes[btnIndex]);
+  });
+});
