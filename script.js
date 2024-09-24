d425d773ae2875e993393aa0c1f7eaa27974245e
berktutu <166663599+berktutu@users.noreply.github.com>
2024-09-24 12:15:22 +0200

    completed the app logic

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
+const button = document.querySelectorAll(".btn");
 const buttonSmall = document.querySelector(".btn-small");
 const buttonMedium = document.querySelector(".btn-medium");
 const buttonLarge = document.querySelector(".btn-large");
 const canvas = document.querySelector(".canvas-container");
 
 // Create a webpage with a 16x16 grid of square divs.
-const numberOfDivs = 16 * 16;
+const smallCanvas = 16 * 16;
+const mediumCanvas = 24 * 24;
+const largeCanvas = 32 * 32;
 
-const test = function () {
-  for (let i = 0; i < numberOfDivs; i++) {
+const smallCanvasDivs = 16;
+const mediumCanvasDivs = 24;
+const largeCanvasDivs = 32;
+
+const fillDivs = function (size, numberOfDivs) {
+  for (let i = 0; i < size; i++) {
     const div = document.createElement("div");
-    div.style.flexBasis = `calc(100% / 16)`;
-    div.style.height = `calc(100% / 16)`;
+    div.style.flexBasis = `calc(100% / ${numberOfDivs})`;
+    div.style.height = `calc(100% / ${numberOfDivs})`;
     canvas.appendChild(div);
 
     div.addEventListener("mouseover", function () {
@@ -18,4 +25,25 @@ const test = function () {
     });
   }
 };
-test();
+fillDivs(smallCanvas, smallCanvasDivs);
+
+const clearCanvas = function () {
+  canvas.innerHTML = "";
+};
+
+button.forEach((btn, i) =>
+  btn.addEventListener("click", function () {
+    if (btn) {
+      clearCanvas();
+    }
+    if (i === 0) {
+      fillDivs(smallCanvas, smallCanvasDivs);
+    }
+    if (i === 1) {
+      fillDivs(mediumCanvas, mediumCanvasDivs);
+    }
+    if (i === 2) {
+      fillDivs(largeCanvas, largeCanvasDivs);
+    }
+  })
+);
