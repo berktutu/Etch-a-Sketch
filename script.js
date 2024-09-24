c18e5dd08bc6ecad3221cb51309564d18f2ac80c
berktutu <166663599+berktutu@users.noreply.github.com>
2024-09-24 13:01:22 +0200

    added color and opacity to squares

--- script.js
+++ script.js
@@ -21,9 +21,22 @@ const fillDivs = function (size) {
   }
 };
 
+const getRandomColor = function () {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r},${g},${b})`;
+};
+
+const getRandomOpacity = function () {
+  const opacity = Math.random();
+  return opacity;
+};
+
 canvas.addEventListener("mouseover", function (event) {
   if (event.target.classList.contains("canvas-div")) {
-    event.target.style.backgroundColor = "black";
+    event.target.style.backgroundColor = getRandomColor();
+    event.target.style.opacity = getRandomOpacity();
   }
 });
 
